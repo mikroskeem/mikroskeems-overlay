8e10bb5d086c90055a67578e8c12c14aeff5e74e
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-11-17 21:42:55 +0200

    Make docker-zfs-plugin actually usable
    Also this means that I've successfully created a working overlay, yay.

--- module.nix
+++ module.nix
@@ -8,6 +8,7 @@
 with lib;
 let
   zfs-cfg = config.services.docker-zfs-plugin;
+  #depot-cfg = config.services.depot;
 in
 {
   options = {
@@ -25,39 +26,61 @@ in
       };
 
       datasets = mkOption {
-        type = types.listOf types.string;
+        type = types.listOf types.str;
         default = [];
         description = "What datasets should be exposed to the plugin";
       };
     };
+
+#    services.depot = {
+#      enable = mkOption {
+#        type = types.bool;
+#        default = false;
+#        description = "Whether to enable depot service";
+#      };
+#
+#      debug = mkOption {
+#        type = types.bool;
+#        default = false;
+#        description = "Whether to enable debug mode";
+#      };
+#    };
   };
 
   config = {
+    assertions = [
+      {
+        assertion = zfs-cfg.enable -> (zfs-cfg.datasets != []);
+        message = "Must specify atleast one dataset when Docker ZFS volume plugin is desired";
+      }
+    ];
+
     systemd.services.docker-zfs-plugin = mkIf zfs-cfg.enable {
       description = "Docker volume plugin for creating persistent volumes as a dedicated zfs dataset.";
-      restart = "always";
+      serviceConfig = {
+        Restart = "on-abnormal";
+
+        ExecStart = "${pkgs.docker-zfs-plugin}/bin/docker-zfs-plugin "
+          + "${if (zfs-cfg.debug) then "--debug" else ""} "
+          + "${concatMapStrings (x: "--dataset-name " + x) zfs-cfg.datasets}";
+      };
 
       before = [ "docker.service" ];
-#      after = ["zfs-mount.service", "zfs-import-cache.service"];
-#      requires = [ "zfs-import-cache.service" ];
       wantedBy = [ "docker.service" ];
-
-      start = "${pkgs.docker-zfs-plugin}/bin/docker-zfs-plugin";
+      requires = [ "zfs.target" ];
+      path = [ pkgs.zfs ];
     };
 
-    systemd.services.depot = mkIf depot.enable {
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Whether to enable docker-zfs-plugin service";
-      };
-
-      debug = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Whether to enable debug mode";
-      };
-
-    };
-  }
+#    systemd.services.depot = mkIf depot-cfg.enable {
+#      descrption = "Depot is a lightweight Maven repository software";
+#      wantedBy = [ "multi-user.target" ];
+#
+#      serviceConfig = {
+#        Restart = "on-abnormal";
+#
+#        # TODO: generate config file
+#        ExecStart = "${pkgs.depot}/bin/depot";
+#      };
+#    };
+  };
 }
